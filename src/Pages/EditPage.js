4c4ef6b20bdf89843b96c436dd6782bcfe949e2c
Morvin-Ian <morvinian@gmail.com>
2022-05-31 12:39:42 +0300

    Update EditPage.js

--- src/Pages/EditPage.js
+++ src/Pages/EditPage.js
@@ -4,11 +4,10 @@ import {useEffect} from 'react'
 
 const EditPage = (posts, editTitle, setEditTitle, editDate, setEditDate, editBody, setEditBody, handleupdates) => {
     const  { id }  = useParams(); 
-    // id comes inform of ":1" therefore to pick the id only id[2] 
-
+  
     //The post arrives as an object format
     const ArrayingPost = posts['posts']
-    const post = ArrayingPost.find(Arraypost => (Arraypost.id).toString() === id[1]);
+    const post = ArrayingPost.find(Arraypost => (Arraypost.id).toString() === id);
     console.log(post["date"])
     // useEffect(()=>{
     //     //Accessed as objects
